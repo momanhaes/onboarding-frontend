f2b1903554509d2ccc23395f81b13fe83e36c5f8
mmanhaes <momanhaes@gmail.com>
2023-05-17 18:44:32 -0300

    feat(estrutura e estilos): insere angular material e organiza estrutura removendo template inicial do angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
+// ANGULAR MODULES
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
+// INTERNAL COMPONENTS
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+
+// INTERNAL IMPORTS
+import { ROUTES } from './app.routes';
+
+const COMPONENTS = [
+  AppComponent,
+  HomeComponent
+];
+
+const ANGULAR_MODULES = [
+  RouterModule.forRoot(ROUTES),
+  BrowserAnimationsModule,
+  ReactiveFormsModule,
+  HttpClientModule,
+  BrowserModule,
+  FormsModule,
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [...COMPONENTS],
+  imports: [...ANGULAR_MODULES],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
