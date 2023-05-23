51e7e92332e2923e19f69e0f91d85e0e4fa2b763
mmanhaes <momanhaes@gmail.com>
2023-05-23 12:41:21 -0300

    feat(table): altera disposição da tabela para que ela fique funcional em telas menores

--- src/app/shared/fragments/table/table.component.ts
+++ src/app/shared/fragments/table/table.component.ts
@@ -26,9 +26,9 @@ export class TableComponent implements AfterViewInit, OnInit {
   public state = 'ready';
 
   public displayedColumns: string[] = [
+    'id',
     'name',
     'email',
-    'cpf',
     'state',
     'cel',
     'edit',
@@ -60,6 +60,7 @@ export class TableComponent implements AfterViewInit, OnInit {
       
       paginator.getRangeLabel = (page: number, pageSize: number, length: number) => {
         if (length === 0 || pageSize === 0) { return `0 de ${length}`; }
+
         length = Math.max(length, 0);
         const startIndex = page * pageSize;
         const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
